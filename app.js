81762df588a8f4136697192943d44cf266c1bec5
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-18 19:48:27 -1000

    post-ish

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 /* jshint esversion:6*/
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  app.render('index');
+});
+
+
+app.post('/:buzzword/:points', (req, res, next) =>{
+  console.log(req.body);
+  next();
+});
+
+app.listen(4958, () => {
+  process.stdout.write('listening on port 4958');
+});
